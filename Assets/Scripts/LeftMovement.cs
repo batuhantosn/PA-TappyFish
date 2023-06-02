1c763145ebfc955fffa8f5bb23e3bea07523d7f3
Batuhan <batuhan.tosun.96@hotmail.com>
2023-06-02 19:26:15 +0300

    LeftMovement Script change

--- Assets/Scripts/LeftMovement.cs
+++ Assets/Scripts/LeftMovement.cs
@@ -7,18 +7,33 @@ public class LeftMovement : MonoBehaviour
     public float speed;
     BoxCollider2D box;
     float groundWidth;
+    float obstacleWidth;
     private void Start()
     {
-        box = GetComponent<BoxCollider2D>();
-        groundWidth = box.size.x;
+        if (gameObject.CompareTag("Ground"))
+        {
+            box = GetComponent<BoxCollider2D>();
+            groundWidth = box.size.x;
+        }
+        else if (gameObject.CompareTag("Obstacle"))
+        {
+            obstacleWidth = GameObject.FindGameObjectWithTag("Column").GetComponent<BoxCollider2D>().size.x;
+        }
+
     }
 
     private void Update()
     {
         transform.position = new Vector2(transform.position.x - speed * Time.deltaTime,transform.position.y);
+
+
         if (transform.position.x <= -groundWidth)
         {
             transform.position = new Vector2(transform.position.x + 2 * groundWidth, transform.position.y);
+        }
+        else if (gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
         }
     }
 
