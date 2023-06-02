1d94f9e65136658d4e74981d5fa440aae4db80f3
Batuhan <batuhan.tosun.96@hotmail.com>
2023-06-02 18:01:28 +0300

    prefab obstacle and gorund move add

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class Fish : MonoBehaviour
 {
     private Rigidbody2D _rb;
-    public float speed = 9f;
+    [SerializeField]private float speed = 9f;
+
+    int angle;
+    int maxangle = 20;
+    int minangle = -60;
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -13,9 +17,29 @@ public class Fish : MonoBehaviour
 
     private void Update()
     {
+
+        FishSwim();
+        FishRotation();
+    }
+
+    public void FishSwim()
+    {
         if (Input.GetMouseButtonDown(0))
         {
-            _rb.velocity = new Vector2(_rb.velocity.x,speed);
+            _rb.velocity = Vector2.zero;
+            _rb.velocity = new Vector2(_rb.velocity.x, speed);
         }
+    }
+    public void FishRotation()
+    {
+        if (_rb.velocity.y > 0 && maxangle > angle)
+        {
+            angle = angle + 4;
+        }
+        else if (_rb.velocity.y < 0 && minangle < angle)
+        {
+            angle = angle - 2;
+        }
+        transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 }
